22ced05ba602715b19b652a82a9733a836fe0bbd
dinaftc <d.fettache@esi-sba.dz>
2023-07-22 14:07:36 +0100

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
